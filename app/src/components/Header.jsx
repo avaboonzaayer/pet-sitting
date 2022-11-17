26e2bbc625ebf2313bd8f687c4d63a7c361e7563
avaboonzaayer <sam.boonzaayer@gmail.com>
2022-11-16 22:39:53 -0700

    created and styled the header component

--- app/src/components/Header.jsx
+++ app/src/components/Header.jsx
@@ -1,5 +1,32 @@
 const Header = () => {
+  return (
+    <div className="header">
+      <h1 className="header__title">
+        Watch My Pet
+      </h1>
 
+      <div className="header__right-btns">
+        <label className="header__sort-by">
+          Sort By:
+          <select className="header__sort-by__drop-down">
+            <option className="header__sort-by__drop-down-option">
+              Lowest Rates
+            </option>
+            <option className="header__sort-by__drop-down-option">
+              Location
+            </option>
+            <option className="header__sort-by__drop-down-option">
+              Best Reviewed
+            </option>
+          </select>
+        </label>
+
+        <button className="header__sign-up">
+          Sign Up
+        </button>
+      </div>
+    </div>
+  )
 }
 
 export default Header;
