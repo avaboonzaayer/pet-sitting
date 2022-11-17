17e8485b9b7b02fe2d99f44e821af064e5a04df7
avaboonzaayer <sam.boonzaayer@gmail.com>
2022-11-16 21:24:26 -0700

    imported header, sitter list, book sitter, and register sitter into app

--- app/src/components/App.jsx
+++ app/src/components/App.jsx
@@ -1,6 +1,11 @@
+import Header from './Header.jsx';
+import SitterList from './SitterList.jsx';
+import BookSitter from './BookSitter.jsx';
+import RegisterSitter from './RegisterSitter.jsx';
+
 function App() {
   return (
-    <div className="main">
+    <div className="app">
       Hello World!
     </div>
   );
